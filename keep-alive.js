d1a512bbc9d16c816ac7f8a158515963e77f1cc6
malikanddeno-ship-it <malikanddeno@gmail.com>
2026-06-12 17:04:54 -0400

    Update keep-alive.js

--- keep-alive.js
+++ keep-alive.js
@@ -1,1 +1,11 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200);
+  res.end("PJA Bot is alive!");
+});
+
+server.listen(process.env.PORT || 3000, () => {
+  console.log("Keep-alive server running on port " + (process.env.PORT || 3000));
+});
 
